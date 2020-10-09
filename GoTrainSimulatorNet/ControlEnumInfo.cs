b4875a6af102d2afecdb976ad127fda059e3043c
tao li <litaodh@outlook.com>
2020-10-09 16:34:21 +0800

    111

--- GoTrainSimulatorNet/ControlEnumInfo.cs
+++ GoTrainSimulatorNet/ControlEnumInfo.cs
@@ -10,7 +10,8 @@ namespace Luster.TrafficSeries
     {
         CameraSignal,
         CaissonSignal,
-        MagneticSteelSignal
+        MagneticSteelSignal,
+        LEDSignal
     }
 
     public enum CameraType
@@ -19,10 +20,43 @@ namespace Luster.TrafficSeries
         Area
     }
 
+    public enum TransmissionMode
+    {
+        Net,
+        SerialPort,
+        SDK
+    }
+
     public class CameraSerie
     {
         public const string DALSALinea2K = "DALSALinea2K";
         public const string DALSALinea4K = "DALSALinea4K";
         public const string DALSANano = "DALSANano";
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum EnableType
+    {
+        FLASH,
+        RAM
+    }
+    /// <summary>
+    /// TTL板卡回复STATE位定义
+    /// </summary>
+    public enum TTLSignalBoardCommandState
+    {
+        //接收到的命令正常     
+        UART_OK_REC = 0x00, 
+        //接收到的命令头错误，即开始标志错误  
+        UART_ERR_REC_HEAD = 0x80,
+        //接收到的命令校验错误
+        UART_ERR_REC_CHECK=0x81,
+        //接收到的命令超出范围
+        UART_ERR_CMD_OUT=0x82,
+        //FLASH操作错误
+        EEPROM_ERR_WROVERT=0x83,
+        //未知
+        UNKNOWN= 0xFF
     }
 }
---
+++ GoTrainSimulatorNet/Signal/LinearCamera2KSignal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luster.TrafficSeries.BLL
+{
+    class LinearCamera2KSignal: CameraSignal
+    {
+        public LinearCamera2KSignal(string signalName):
+            base(signalName, CameraType.Linear,CameraSerie.DALSALinea2K,
+                SignalType.CameraSignal,TransmissionMode.SerialPort)
+        {
+            switch (this.signalName)
+            {
+                case "RS485-4":
+                    this.signalContext = "";
+                    break;
+                case "RS485-5":
+                    this.signalContext = "";
+                    break;
+            }
+        }
+    }
+}
